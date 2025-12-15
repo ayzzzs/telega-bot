e2b59f92f4480dc0e8304eba6a81b199148aa6a5
ayzzzs <iliyaobirin@gmail.com>
2025-12-15 20:06:32 +0300

    Add files via upload

---
+++ bbbbb/Project/Core/Repositories/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using bbbbb.Project.Core.Entities;
+
+namespace bbbbb.Project.Core.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<ToDoUser?> GetUserAsync(Guid userId, CancellationToken cancellationToken);
+        Task<ToDoUser?> GetUserByTelegramUserIdAsync(long telegramUserId, CancellationToken cancellationToken);
+        Task AddAsync(ToDoUser user, CancellationToken cancellationToken);
+    }
+}
+
