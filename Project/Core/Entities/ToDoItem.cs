568f427a476ec27ea91738212c26e9acd4e4299c
ayzzzs <iliyaobirin@gmail.com>
2026-02-03 17:38:57 +0300

    Add files via upload

---
+++ Project/Core/Entities/ToDoItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bbbbb.Project.Core.Entities
+{
+    public enum ToDoState
+    {
+        Active,
+        Completed
+    }
+    public class ToDoItem
+    {
+        public Guid Id { get; init; } = Guid.NewGuid();
+        public ToDoUser User { get; init; }
+        public string Name { get; init; }
+        public ToDoState State { get; private set; } = ToDoState.Active;
+
+        public ToDoItem(ToDoUser user, string name)
+        {
+            User = user;
+            Name = name;
+        }
+
+        public void MarkCompleted()
+        {
+            State = ToDoState.Completed;
+        }
+    }
+}
---
+++ Project/Core/Repositories/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using bbbbb.Project.Core.Entities;
+
+namespace bbbbb.Project.Core.Repositories
+{
+    public interface IUserRepository
+    {
+        Task AddAsync(ToDoUser user, CancellationToken ct);
+        Task<ToDoUser?> GetByTelegramIdAsync(long telegramId, CancellationToken ct);
+    }
+}
+
